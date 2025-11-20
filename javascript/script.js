a84e82013f8048e8abc1ecd6a745e68d1c2d7542
kalyesubulawilfredkevin <kwilfred151@gmail.com>
2025-11-20 19:17:44 +0300

    work

--- javascript/script.js
+++ javascript/script.js
@@ -1,4 +1,14 @@
 // =========
+// navbar
+document.addEventListener("DOMContentLoaded", function () {
+  const navbarToggler = document.querySelector(".navbar-toggler");
+  const navbarCollapse = document.getElementById("navbarNav");
+
+  navbarToggler.addEventListener("click", function () {
+    navbarCollapse.classList.toggle("show");
+  });
+});
+
 //=====Gallery====//
 document.addEventListener("DOMContentLoaded", function () {
   // Gets all gallery images
